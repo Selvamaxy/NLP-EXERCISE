a83b1aa5c55c0e7e642e0493757357b20ee104e1
selvakumar V.R.S <157942095+Selvamaxy@users.noreply.github.com>
2026-01-27 14:46:10 +0530

    Add files via upload
    Tokenizeing text

---
+++ ex2.py
@@ -0,0 +1,12 @@
+
+import nltk
+from nltk.tokenize import word_tokenize
+
+nltk.download('punkt')
+nltk.download('punkt_tab')
+
+text = "Natural Language Processing"
+tokens = word_tokenize(text)
+
+print("The word Tokens are:")
+print(tokens)
